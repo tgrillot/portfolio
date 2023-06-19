8e1279da260a7d927d7c764c5aff0d8e154499cc
Travis.Grillot@dtpartners.com <Travis.Grillot@dtpartners.com>
2023-06-19 13:00:20 -0400

    responsive layout

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.88.187']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.88.187','192.168.27.127']
 
 
 # Application definition
